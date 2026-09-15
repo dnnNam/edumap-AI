210de267c0ff2d80dd8dae3d31ec2186eae8adad
Nguyễn Đại Nam <dainamnguyen3@gmail.com>
2026-09-15 18:15:39 +0700

    config axios

---
+++ src/config/http.ts
@@ -0,0 +1,20 @@
+import type { AxiosInstance } from "axios"
+import axios from "axios"
+
+class Http {
+  instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL:import.meta.env.VITE_BASE_URL ,
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  
+  }
+}
+
+const http = new Http().instance
+
+export default http
