181dd4e92170d23249ea9e4aa1212e696177b948
Nguyễn Đại Nam <dainamnguyen3@gmail.com>
2026-09-24 15:25:01 +0700

    config file http.ts

--- src/config/http.ts
+++ src/config/http.ts
@@ -12,7 +12,7 @@ class Http {
   instance: AxiosInstance
   constructor() {
     this.instance = axios.create({
-      baseURL: import.meta.env.VITE_BASE_URL,
+      baseURL: import.meta.env.VITE_API_URL,
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json',
